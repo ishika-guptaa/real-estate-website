1d81e57266c53dbaa69d5d52a1eed9a1ab9bcb97
Ishika Gupta <heyishika02april@gmail.com>
2024-08-04 03:00:23 +0530

    redirect uri updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Auth0Provider } from '@auth0/auth0-react'
-import { MantineProvider } from '@mantine/core'
-import '@mantine/core/styles.css'
-import '@mantine/dates/styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-      domain='dev-joftssxywm531mxq.us.auth0.com'
-      clientId='qvQiTtpeuY3qXRwWgSpkgeCCCW4LMOc9'
+      domain="dev-joftssxywm531mxq.us.auth0.com"
+      clientId="qvQiTtpeuY3qXRwWgSpkgeCCCW4LMOc9"
       authorizationParams={{
-        redirect_uri: 'http://localhost:5173'
-        
+        redirect_uri: "https://real-estate-website-fr.vercel.app",
       }}
-      audience='http://localhost:8000'
-      scope='openid profile email'
+      audience="http://localhost:8000"
+      scope="openid profile email"
     >
       <MantineProvider>
         <App />
       </MantineProvider>
-
     </Auth0Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
