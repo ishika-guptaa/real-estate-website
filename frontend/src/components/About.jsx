85b8955519058c21135d459ac78cda5745da6975
Ishika Gupta <heyishika02april@gmail.com>
2024-08-09 23:23:09 +0530

    new changes

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from "react";
 import aboutImg from "../assets/about.jpg";
 import { RiDoubleQuotesL } from "react-icons/ri";
-import CountUp from 'react-countup'
 
 const About = () => {
-    const statistics = [
-        { label: 'Happy Clients', value: 12 },
-        { label: 'Different Clients', value: 3 },
-        { label: 'Projects Clients', value: 45 },
-    ]
+
     const [isVisible, setIsVisible] = useState(false)
 
     useEffect(() => {
@@ -51,30 +46,12 @@ const About = () => {
                 <div className="flex-1 flex justify-center flex-col
                 ">
                     <span className="medium-18">Unveiling Our Journey</span>
-                    <h2 className="h2">Lorem ipsum dolor sit, amet consectetur adipisicing.</h2>
+                    <h2 className="h2">Our Commitment Crafting Extraordinary Real Estate Experiences</h2>
                     <p className="py-5">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
                         odit necessitatibus expedita laboriosam maxime iste quis dolores
-                        cupiditate autem voluptatibus.
+                        cupiditate autem voluptatibus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, placeat?
                     </p>
-                    {/* statistics container */}
-                    <div className="flex flex-wrap gap-4">
-                        {statistics.map((statistic, index) => (
-                            <div key={index} className="bg-primary p-4 rounded-lg">
-                                <div className="flex items-center gap-1">
-                                    <CountUp start={isVisible?0:null} end={statistic.value} duration={10
-                                } delay={3}>
-                                {({countUpRef})=>(
-                                    <h3 ref={countUpRef} className="text-2xl font-semibold"></h3>
-                                )}
-                                   
-                                </CountUp>
-                                <h4 className="bold-22">k+</h4>
-                                </div>
-                                <p>{statistic.label}</p>
-                            </div>
-                        ))}
-                    </div>
                 </div>
             </div>
         </section>
